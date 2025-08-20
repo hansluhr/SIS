6a04b0e451da43cbc68249c91c9afbbeaffa2b8d
hansluhr <gablins@yahoo.com.br>
2025-08-20 18:30:01 -0300

    Procedimentos automático.

--- SIH/funcao_tratamento_empilhamento_sih.R
+++ SIH/funcao_tratamento_empilhamento_sih.R
@@ -1199,92 +1199,20 @@ tratar_sih <- function(data) {
   
 
 #Aplicando label em procedimentos -----------------------------------------------------------
-procedimentos <- fread("C:/Users/gabli/Desktop/r/SIH/bases_auxiliares/TB_SIGTAB.csv", sep = ";") |>
-    rename(proc = value) |>
-    mutate(proc = proc |> str_to_title() |> as_factor() )
+# procedimentos <- fread("C:/Users/gabli/Desktop/r/SIH/bases_auxiliares/TB_SIGTAB.csv", sep = ";") |>
+#     rename(proc = value) |>
+#     mutate(proc = proc |> str_to_title() |> as_factor() )
+
   
-  
-  
-  
-  
+#Importação de função com código de    
 source("C:/Users/gabli/Dropbox/Ipea/Atlas/Rotinas/SIS/SIH/funcao_cod_procedimentos_SUS.R")
-
-  
-  FAZER 
-  
-  
-  
-  
-  
-  
-#Importando pacotes
-  library(RCurl)
-  library(stringr)
-  
-  #URL do ftp
-  ftp_base <- "ftp://ftp2.datasus.gov.br/pub/sistemas/tup/downloads/"
-  
-  #Lista de todos os arquivos no FTP
-  arquivos <- getURL(
-    ftp_base, 
-    dirlistonly = TRUE,
-    ftp.use.epsv = FALSE) |> 
-    str_split("\r?\n") |> 
-    unlist()
-  
-  #Quero a tabela de procedimentos. 
-  #O arquivo começa com TabelaUnificada_. Faço um filtro para manter arquivos começanco com TabelaUnificada_
-  arquivos <- arquivos[str_detect(arquivos, "^TabelaUnificada_.*\\.zip$")]
-  
-  #Ordenação. O mais novo aparece primeiro. 
-  #A data de upload aparece no nome do arquivo. Faz sentido fazer isso.
-  arquivos <- sort(arquivos, decreasing = TRUE)
-  
-  #Pega o mais recente. O primeiro da lista.
-  arquivos <- arquivos[1]
-  message("Arquivo mais recente encontrado no FTP: ", arquivos)
-  
-  #Faz download do arquivo mais recente.
-  destino <- file.path(tempdir(), arquivos) #Pasta temporária para armazenar o zip.
-  download.file(
-    url = paste0(ftp_base, arquivos),
-    destfile = destino,
-    mode = "wb")
-  
-  #Lê o arquivo tb_procedimento.txt de dentro do zip
-  procedimentos <- read.delim(
-    unz(destino, "tb_procedimento.txt"),
-    header = FALSE,
-    encoding = "latin1",
-    col.names = c("cod_proc") ) |>
-    dplyr::mutate(
-      cod = str_sub(cod_proc, 1, 9) |> as.integer(),      #Pega os 9 primeiros dígitos do código. No dbcs do sih o código do procedimento está com 9 dígitos
-      resto  = str_sub(cod_proc, 11),         # Extrai o texto a partir do 11º dígito.
-      #remove tudo a partir de: [espaços]* + dígito + letra(s) + muitos dígitos
-      proc = resto |>
-        str_replace("\\s*[0-9][A-Z]{1,2}\\d{6,}.*$", "") |>
-        #fallback: se não houver esse marcador, cortar antes de blocos de 10+ dígitos
-        (\(x) ifelse(x == resto, str_replace(resto, "\\d{10,}.*$", ""), x)) () |>
-        
-        str_trim() |> #Remove espaçoes excedentes.
-        
-        str_to_title() |> #Primeira letra maiúscula
-        
-        forcats::as_factor() ) |> #Transforma em factor.
-    dplyr::select(-c(resto,cod_proc) ) |>
-    data.table::setDT()
-
-  
-#left_joit label de procedimento solicitado
-data[procedimentos, def_proc_solic := i.proc, on = .(PROC_SOLIC = cod)] 
-#left_joit label de procedimento realizado
-data[procedimentos, def_proc_rea   := i.proc, on = .(PROC_REA = cod)]
-
-  
-  
-rm(list = setdiff( c("ftp_base", "destino", "arquivos", "procedimentos") , c("data") ))  
-  
-  
+ 
+   #left_joit label de procedimento solicitado
+  data[procedimentos, def_proc_solic := i.proc, on = .(PROC_SOLIC = cod)] 
+  #left_joit label de procedimento realizado
+  data[procedimentos, def_proc_rea   := i.proc, on = .(PROC_REA = cod)]
+rm(list = setdiff( c("ftp_base", "destino", "arquivos", "procedimentos") , c("data") ))   
+ 
   
    
 # #Municípios --------------------------------------------------------------
