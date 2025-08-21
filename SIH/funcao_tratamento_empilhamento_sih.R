ef73f882d9440000960eb9a9394d1ddef0958bae
hansluhr <gablins@yahoo.com.br>
2025-08-21 20:15:01 -0300

    base munic através de função própria.

--- SIH/funcao_tratamento_empilhamento_sih.R
+++ SIH/funcao_tratamento_empilhamento_sih.R
@@ -1206,10 +1206,9 @@ tratar_sih <- function(data) {
 # source("https://raw.githubusercontent.com/hansluhr/SIS/refs/heads/main/SIH/funcao_cod_procedimentos_SUS.R")
  
   #A importação da tabela com procediemtnos é feita através de source na rotina de criação da base.
+  #Inicialmente era feita através da tabela tb_sigtab
   
-  
-  
-   #left_joit label de procedimento solicitado
+     #left_joit label de procedimento solicitado
   data[procedimentos, def_proc_solic := i.proc, on = .(PROC_SOLIC = cod)] 
   #left_joit label de procedimento realizado
   data[procedimentos, def_proc_rea   := i.proc, on = .(PROC_REA = cod)]
@@ -1218,50 +1217,50 @@ tratar_sih <- function(data) {
   
    
 # #Municípios --------------------------------------------------------------
-  #Download das informações sobre municípios.
-  munics <- geobr::read_municipality(year = 2022) |> as_tibble() |>
-    #Código dos municípios com 6 dígitos.
-    mutate(code_muni = code_muni |> str_sub(start = 1, end = 6),
-           #Transforma em factor
-           across( c(code_muni, name_state, code_state), ~ as_factor(.x) ) ) |>
-    #Excluindo variáveis não utilizadas.
-    select(!c(code_region, geom)) |>
-    #Elimando geo
-    sf::st_drop_geometry(data_all) |>
-
-    ###Adiciona informações sobre os municípios ignorados
-    bind_rows(
-
-      tribble(~code_muni,~name_muni,~code_state,~abbrev_state,~name_state, ~name_region,
-              "000000", "Ignorado ou exterior",    "00", "IGN", "Ignorado ou exterior", "Ignorado ou exterior",
-              "110000", "Município ignorado - RO", "11",  "RO", "Rondônia", "Norte",
-              "130000", "Município ignorado - AM", "13",  "AM", "Amazonas", "Norte",
-              "150000", "Município ignorado - PA", "15",  "PA", "Pará", "Norte",
-              "210000", "Município ignorado - MA", "21",  "MA", "Maranhão", "Nordeste",
-              "170000", "Município ignorado - TO", "17",  "TO", "Tocantins", "Norte",
-              "240000", "Município ignorado - RN", "24",  "RN", "Rio Grande do Norte", "Nordeste",
-              "260000" ,"Município ignorado - PE", "26",  "PE", "Pernambuco", "Nordeste",
-              "280000", "Município ignorado - SE", "28",  "SE", "Sergipe", "Nordeste",
-              "310000", "Município ignorado - MG", "31",  "MG", "Minas Gerais", "Sudeste",
-              "330000", "Município ignorado - RJ", "33",  "RJ", "Rio de Janeiro", "Sudeste",
-              "410000", "Município ignorado - PR", "41",  "PR", "Paraná", "Sul",
-              "430000", "Município ignorado - RS", "43",  "RS", "Rio Grande do Sul", "Sul",
-              "510000", "Município ignorado - MT", "51",  "MT", "Mato Grosso", "Centro Oeste",
-              "520000", "Município ignorado - GO", "52",  "GO", "Goiás", "Centro Oeste",
-              "120000", "Município ignorado - AC", "12",  "AC", "Acre", "Norte",
-              "140000", "Município ignorado - RR", "14",  "RR", "Roraima", "Norte",
-              "160000", "Município ignorado - AP", "16",  "AP", "Amapá",  "Norte",
-              "220000", "Município ignorado - PI", "22",  "PI", "Piauí", "Nordeste",
-              "230000", "Município ignorado - CE", "23",  "CE", "Ceará",  "Nordeste",
-              "250000", "Município ignorado - PB", "25",  "PB", "Paraíba","Nordeste",
-              "270000", "Município ignorado - AL", "27",  "AL", "Alagoas", "Nordeste",
-              "290000", "Município ignorado - BA", "29",  "BA", "Bahia", "Nordeste",
-              "320000", "Município ignorado - ES", "32",  "ES", "Espírito Santo", "Sul",
-              "350000", "Município ignorado - SP", "35",  "SP", "São Paulo", "Sudeste",
-              "420000", "Município ignorado - SC", "42",  "SC", "Santa Catarina", "Sul",
-              "500000", "Município ignorado - MS", "50",  "MS", "Mato Grosso do Sul", "Sul") ) |>
-    #Conversão para factor. Exceto código do município.
-    mutate(across(.cols = !c(code_muni), as_factor  ) )
+  # #Download das informações sobre municípios.
+  # munics <- geobr::read_municipality(year = 2022) |> as_tibble() |>
+  #   #Código dos municípios com 6 dígitos.
+  #   mutate(code_muni = code_muni |> str_sub(start = 1, end = 6),
+  #          #Transforma em factor
+  #          across( c(code_muni, name_state, code_state), ~ as_factor(.x) ) ) |>
+  #   #Excluindo variáveis não utilizadas.
+  #   select(!c(code_region, geom)) |>
+  #   #Elimando geo
+  #   sf::st_drop_geometry(data_all) |>
+  # 
+  #   ###Adiciona informações sobre os municípios ignorados
+  #   bind_rows(
+  # 
+  #     tribble(~code_muni,~name_muni,~code_state,~abbrev_state,~name_state, ~name_region,
+  #             "000000", "Ignorado ou exterior",    "00", "IGN", "Ignorado ou exterior", "Ignorado ou exterior",
+  #             "110000", "Município ignorado - RO", "11",  "RO", "Rondônia", "Norte",
+  #             "130000", "Município ignorado - AM", "13",  "AM", "Amazonas", "Norte",
+  #             "150000", "Município ignorado - PA", "15",  "PA", "Pará", "Norte",
+  #             "210000", "Município ignorado - MA", "21",  "MA", "Maranhão", "Nordeste",
+  #             "170000", "Município ignorado - TO", "17",  "TO", "Tocantins", "Norte",
+  #             "240000", "Município ignorado - RN", "24",  "RN", "Rio Grande do Norte", "Nordeste",
+  #             "260000" ,"Município ignorado - PE", "26",  "PE", "Pernambuco", "Nordeste",
+  #             "280000", "Município ignorado - SE", "28",  "SE", "Sergipe", "Nordeste",
+  #             "310000", "Município ignorado - MG", "31",  "MG", "Minas Gerais", "Sudeste",
+  #             "330000", "Município ignorado - RJ", "33",  "RJ", "Rio de Janeiro", "Sudeste",
+  #             "410000", "Município ignorado - PR", "41",  "PR", "Paraná", "Sul",
+  #             "430000", "Município ignorado - RS", "43",  "RS", "Rio Grande do Sul", "Sul",
+  #             "510000", "Município ignorado - MT", "51",  "MT", "Mato Grosso", "Centro Oeste",
+  #             "520000", "Município ignorado - GO", "52",  "GO", "Goiás", "Centro Oeste",
+  #             "120000", "Município ignorado - AC", "12",  "AC", "Acre", "Norte",
+  #             "140000", "Município ignorado - RR", "14",  "RR", "Roraima", "Norte",
+  #             "160000", "Município ignorado - AP", "16",  "AP", "Amapá",  "Norte",
+  #             "220000", "Município ignorado - PI", "22",  "PI", "Piauí", "Nordeste",
+  #             "230000", "Município ignorado - CE", "23",  "CE", "Ceará",  "Nordeste",
+  #             "250000", "Município ignorado - PB", "25",  "PB", "Paraíba","Nordeste",
+  #             "270000", "Município ignorado - AL", "27",  "AL", "Alagoas", "Nordeste",
+  #             "290000", "Município ignorado - BA", "29",  "BA", "Bahia", "Nordeste",
+  #             "320000", "Município ignorado - ES", "32",  "ES", "Espírito Santo", "Sul",
+  #             "350000", "Município ignorado - SP", "35",  "SP", "São Paulo", "Sudeste",
+  #             "420000", "Município ignorado - SC", "42",  "SC", "Santa Catarina", "Sul",
+  #             "500000", "Município ignorado - MS", "50",  "MS", "Mato Grosso do Sul", "Sul") ) |>
+  #   #Conversão para factor. Exceto código do município.
+  #   mutate(across(.cols = !c(code_muni), as_factor  ) )
 
 
   #left_Join com município de residência
