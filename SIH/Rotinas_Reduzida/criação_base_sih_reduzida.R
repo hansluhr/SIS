15e9e3189b684ff77fc67eb42da13391243de733
hansluhr <gablins@yahoo.com.br>
2025-08-28 14:40:48 -0300

    Rename para criação_base_sih_reduzida.

--- SIH/Rotinas_Reduzida/criação_base_sih_reduzida.R
+++ SIH/Rotinas_Reduzida/criação_base_sih_reduzida.R
@@ -4,19 +4,19 @@ library(future.apply)
 library(data.table)
 library(duckdb)
 
-here::i_am("SIH/Rotinas/criação_base_sih.R")
+here::i_am("SIH/Rotinas_Reduzida/criação_base_sih.R")
 
 # Rotina de criação da base SIH -------------------------------------------
 #Chamar função para importar arquivos DBCs do FTP DataSuS 
-source("https://raw.githubusercontent.com/hansluhr/SIS/refs/heads/main/SIH/Rotinas/sih_baixar_dbc_ftp.R")
+source("https://raw.githubusercontent.com/hansluhr/SIS/refs/heads/main/SIH/Rotinas_comuns_sih/sih_baixar_dbc_ftp.R")
 
 #Pasta onde os arquivos DBCs do SIH serão salvos.
-#caminho_dbc <- "C:/Users/gabli/Desktop/r/SIH/dbc"
 #Baixar arquivos dbcs SIH
-baixar_dbc_sih(anos = c(2008:2025), 
+baixar_dbc_sih(anos = c(2025), 
                    meses = c(1:12), 
-                   ufs = c("AC","AP","TO"), 
-                   destino = here::here("Bases/sih/dbc") )
+                   ufs = c("AC"), 
+                   destino = here::here("Bases/sih/dbc_reduzida"),
+                   tipo  = "reduzida")
 rm(baixar_dbc_sih)
 
 
